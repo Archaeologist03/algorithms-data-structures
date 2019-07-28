2cea41bf7fe946913888573995166bcc57854a49
Archaeologist03 <omniiknight@hotmail.com>
2019-07-28 23:50:05 +0200

    Adding Tribonacci sequence alg.

---
+++ codewars/tribonacciSequence.js
@@ -0,0 +1,17 @@
+function tribonacci(signature,n){
+  if (n < 3) return signature.slice(0, n)
+  
+  const last3Arr = signature.slice((signature.length - 3), (signature.length));
+  
+  while (n > 3) {
+    let lastVal = last3Arr.reduce((total, curVal) => total + curVal);
+  
+    n = n - 1
+    return tribonacci([...signature, lastVal], n)
+  }
+  
+  return signature
+}
+  
+tribonacci([1,1,1],10)
+tribonacci([0,0,1],10) 
