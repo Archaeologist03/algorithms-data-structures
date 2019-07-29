a1478caca5d95677068ff9d9be441654c114abf0
Archaeologist03 <omniiknight@hotmail.com>
2019-07-29 21:09:50 +0200

    configuring vs code on lap tryout.. - tribonaci info

--- codewars/tribonacciSequence.js
+++ codewars/tribonacciSequence.js
@@ -1,17 +1,20 @@
-function tribonacci(signature,n){
-  if (n < 3) return signature.slice(0, n)
-  
-  const last3Arr = signature.slice((signature.length - 3), (signature.length));
-  
+// 6kyu
+// Recursive solution for fibonacci alike problem.
+
+function tribonacci(signature, n) {
+  if (n < 3) return signature.slice(0, n);
+
+  const last3Arr = signature.slice(signature.length - 3, signature.length);
+
   while (n > 3) {
     let lastVal = last3Arr.reduce((total, curVal) => total + curVal);
-  
-    n = n - 1
-    return tribonacci([...signature, lastVal], n)
+
+    n = n - 1;
+    return tribonacci([...signature, lastVal], n);
   }
-  
-  return signature
+
+  return signature;
 }
-  
-tribonacci([1,1,1],10)
-tribonacci([0,0,1],10) 
+
+tribonacci([1, 1, 1], 10);
+tribonacci([0, 0, 1], 10);
