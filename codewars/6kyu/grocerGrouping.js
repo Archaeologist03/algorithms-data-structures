29d82eb65776a7dbf31755cb0f466790ab7c3ad7
Archaeologist03 <omniiknight@hotmail.com>
2020-06-27 01:06:01 +0200

    Grocer grouping - working on ( some tests failing - no examples in istructons)

--- codewars/6kyu/grocerGrouping.js
+++ codewars/6kyu/grocerGrouping.js
@@ -1,8 +1,31 @@
 // Group items by category
 // Fruit, Meat, Other, Vegetable
 function solution(input) {
-  
+  // Return only stuff after _ in a str
+  const removedCategoryPrefix = (arr) =>
+    arr.map((item) => item.split('_').pop()).sort();
+  // Find specific category, removes categorey from found str and sorts it out.
+  const findByCategory = (arr, checker) => {
+    const categoryArr = arr.filter((item, i) => {
+      if (item.includes(checker)) {
+        arr.splice(i, 1);
+        return true;
+      }
+    });
 
+    return removedCategoryPrefix(categoryArr);
+  };
+
+  const splited = input.split(',');
+
+  const fruit = findByCategory(splited, 'fruit_');
+  const meat = findByCategory(splited, 'meat_');
+  const vegetable = findByCategory(splited, 'vegetable_');
+  const other = removedCategoryPrefix(splited);
+
+  const separator = '\n';
+
+  return `fruit:${fruit}${separator}meat:${meat}${separator}other:${other}${separator}vegetable:${vegetable}`;
 }
 
 solution(
