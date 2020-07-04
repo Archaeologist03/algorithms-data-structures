71ede2cc7fdd7fc0ec4c3b252a135f4be814dd9a
Archaeologist03 <omniiknight@hotmail.com>
2020-07-04 16:51:42 +0200

    ROT13 - 5kyu - cipher, charCode, strings casing.(completed).

--- codewars/5kyu/rot13.js
+++ codewars/5kyu/rot13.js
@@ -1,30 +1,24 @@
 function rot13(message) {
   const changedValuesArr = [];
+
   for (let i = 0; i < message.length; i++) {
-    const charCode = message.charCodeAt(i);
+    const charCode = message.toUpperCase().charCodeAt(i);
 
-    if (
-      (charCode >= 97 && charCode <= 122) ||
-      (charCode >= 65 && charCode <= 90)
-    ) {
+    // in uppercase alphabet range
+    if (charCode >= 65 && charCode <= 90) {
       let newValue;
-      if (charCode - 13 < 65 || charCode - 13 < 97) {
-        console.log(
-          charCode,
-          charCode - 13,
-          message[i],
-          String.fromCharCode(charCode - 13),
-        );
 
-        const reminder = charCode - 65
-
-        newValue = String.fromCharCode(charCode + 13);
-
-        console.log(newValue);
+      // 2nd half of alphabet - goes above z with a 13 chars shift
+      if (charCode + 13 > 90) {
+        const reminder = charCode + 13 - 90;
+        newValue = String.fromCharCode(65 + reminder - 1);
       } else {
-        newValue = String.fromCharCode(charCode - 13);
+        newValue = String.fromCharCode(charCode + 13);
       }
-      changedValuesArr.push(newValue);
+      // turn char to lowercase if org msg char was lowercased.
+      message.charCodeAt(i) > 90
+        ? changedValuesArr.push(newValue.toLowerCase())
+        : changedValuesArr.push(newValue);
     } else {
       changedValuesArr.push(message[i]);
     }
@@ -32,10 +26,6 @@ function rot13(message) {
   return changedValuesArr.join('');
 }
 
-// rot13('test'); // "Input: test , Expected Output: grfg ,
-const res = rot13('Test1$'); // "Input: Test , Expected Output: Grfg
+rot13('test'); // "Input: test , Expected Output: grfg ,
+const res = rot13('Test'); // "Input: Test , Expected Output: Grfg
 console.log(res);
-
-//97 - 122
-// 65 - 90
-console.log('e'.charCodeAt(0));
